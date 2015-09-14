2df50e4d8cfe7e334e173bbe9afb4200f6fac77b
Antoine LeBel <antoine.lebel.65@gmail.com>
2015-09-14 19:00:21 -0400

    added

--- wolf.sh
+++ wolf.sh
@@ -1,5 +1,6 @@
 #makefile (avec makefile si possible)
 #cat ssh, compilation, printing des process
 tonpass="XXXXXXX"
-cat teste.sh | sshpass -p$tonpass ssh -o StrictHostKeyChecking=no cf491141@malt.labunix.uqam.ca "cat > teste; chmod a+x teste; ./teste"
+tonfichier="myfile.out"
+cat $tonfichier | sshpass -p$tonpass ssh -o StrictHostKeyChecking=no cf491141@malt.labunix.uqam.ca "cat > $tonfichier; chmod a+x $tonfichier; ./$tonfichier"
 
