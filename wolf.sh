db541a858b035b2fa7c8a117195c16c08577765d
philtrep <phil.trepanier@gmail.com>
2015-09-15 12:28:09 -0400

    Enleve espace pas rapport

--- wolf.sh
+++ wolf.sh
@@ -6,4 +6,4 @@ infile="$3"
 host=malt.labunix.uqam.ca
 tmp=/home/$user/tmp/
 outsuffix=.out
-cat $infile | sshpass -p $pass ssh $user@$host "mkdir -p $tmp;cat > $tmp$infile;gcc $tmp$infile -Wall -o $tmp$infi    le$outsuffix; $tmp$infile$outsuffix; rm $tmp$infile $tmp$infile$outsuffix;"
+cat $infile | sshpass -p $pass ssh $user@$host "mkdir -p $tmp;cat > $tmp$infile;gcc $tmp$infile -Wall -o $tmp$infile$outsuffix; $tmp$infile$outsuffix; rm $tmp$infile $tmp$infile$outsuffix;"
