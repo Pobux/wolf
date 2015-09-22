44e23c57df0a9b3bfc953f0c8f4ffec2500d10ac
Antoine LeBel <antoine.lebel.65@gmail.com>
2015-09-22 09:34:27 -0400

    amelioration UX

---
+++ teste.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("woo");
+    return 0;
+}
--- wolf.sh
+++ wolf.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
+echo "Entre tes infos pour te connecter en ssh "
+read -e -p "User : " user
+read -s -p "Password : " pass 
+echo ""
+read -e -p "File : " infile
 
-user="$1"
-pass="$2"
-infile="$3"
 host=malt.labunix.uqam.ca
 tmp=/home/$user/tmp/
 outsuffix=.out
+
+echo "Connection sur $host avec le user $user pour le fichier $infile"
+echo ""
+echo "Resultat:"
+echo ""
+
 cat $infile | sshpass -p $pass ssh $user@$host "mkdir -p $tmp;cat > $tmp$infile;gcc $tmp$infile -Wall -o $tmp$infile$outsuffix; $tmp$infile$outsuffix; rm $tmp$infile $tmp$infile$outsuffix;"
+
+echo ""
+echo ""
+echo "Fin de la transaction"
